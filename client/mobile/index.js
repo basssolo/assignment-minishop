e67954fe4f7f7247ec32a902eb89206a689a1aa4
Pasi Häkkinen <pasi.hakkinen@hotmail.fi>
2023-10-26 10:33:29 +0300

    Correctly import App for root

--- client/mobile/index.js
+++ client/mobile/index.js
@@ -1,9 +1,10 @@
 import { AppRegistry } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
-
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { NavigationContainer } from '@react-navigation/native';
+
+// import App from './App';
+import { name as appName } from './app.json';
+import App from './src/app/App';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
